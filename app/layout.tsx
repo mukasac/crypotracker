3d75f04383cd2c4a41b520995e8919d0f9822754
Rose-xlab <jennifernanyombi1@gmail.com>
2025-01-10 21:07:16 +0100

    vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
